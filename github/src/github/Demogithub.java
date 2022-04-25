ab7a8453c85ddc0f78d9a9ba8139da4824c55e77
hp <hp@LAPTOP-9E2F6B48>
2022-04-25 12:15:50 +0530

    by dhananjay25

--- github/src/github/Demogithub.java
+++ github/src/github/Demogithub.java
@@ -2,6 +2,11 @@ package github;
 
 public class Demogithub {
 
+	
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
